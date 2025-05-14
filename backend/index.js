e332110058d462610c02c458283afeb197327f50
Adam Oporski <aoporski28@gmail.com>
2025-05-14 14:54:13 +0200

    routes_added

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const db = require("./models");
 
 const app = express();
+const loginRoutes = require("../backend/routes/login");
+const oauthRoutes = require("../backend/routes/oauth");
+const registerRoutes = require("../backend/routes/register");
+const tokenRoutes = require("../backend/routes/token");
 
 const PORT = 3000;
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+app.use("/api/register", registerRoutes);
+app.use("/api/login", loginRoutes);
+app.use("/api/oauth", oauthRoutes);
+app.use("/api/token", tokenRoutes);
 
 const start = async () => {
   try {
