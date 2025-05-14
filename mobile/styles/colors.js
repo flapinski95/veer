e94055a53200eb93f530ba52087cd6150fb92be7
gjastrzebska203 <g.jastrzebska.203@studms.ug.edu.pl>
2025-05-14 17:21:02 +0200

    prettier styles

---
+++ mobile/styles/colors.js
@@ -0,0 +1,20 @@
+const lightTheme = {
+  background: '#F5F5F5',
+  text: '#000000',
+  surface: '#ffffff',
+  accent: '#007bff',
+};
+
+const darkTheme = {
+  background: 'grey',
+  text: '#ffffff',
+  surface: '#1e1e1e',
+  accent: '#00bfff',
+};
+
+const Colors = {
+  light: lightTheme,
+  dark: darkTheme,
+};
+
+export default Colors;
