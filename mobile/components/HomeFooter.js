0a25a40ab7ae2a1aa071ce74a73c4a41eb9d692c
gjastrzebska203 <g.jastrzebska.203@studms.ug.edu.pl>
2025-05-14 14:34:52 +0200

    buttons

--- mobile/components/HomeFooter.js
+++ mobile/components/HomeFooter.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {View, Text, Button, Alert} from 'react-native';
+import styles from '../styles/styles';
 
 export default function HomeFooter() {
   return (
-    <View style={{padding: '4px'}}>
+    <View  style={styles.fixToText}>
       <Button
         title="Saved"
         onPress={() => Alert.alert('Left button pressed')}
