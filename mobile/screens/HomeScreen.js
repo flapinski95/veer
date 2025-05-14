fda53d0ff2f05be6e90f2542e6e13b3bf3cba3b7
gjastrzebska203 <g.jastrzebska.203@studms.ug.edu.pl>
2025-05-14 13:13:08 +0200

    home screen i tip

--- mobile/screens/HomeScreen.js
+++ mobile/screens/HomeScreen.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useTheme } from '../contexts/ThemeContext';
 import ThemeSwitch from '../components/themeSwitch';
+import Tip from '../components/tip'
 
 export default function HomeScreen({ navigation }) {
   const { colors } = useTheme();
   return (
     <View style={{ flex: 1, backgroundColor: colors.background, padding: 20 }}>
-      <Text style={{ color: colors.text, fontSize: 24 }}>Home Screen</Text>
       <ThemeSwitch />
+      <Text style={{ color: colors.text, fontSize: 24 }}>Home Screen</Text>
+      <Tip />
     </View>
   );
 }
