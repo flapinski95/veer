5c1622e07d47ee9e9bcdf791a82680ff36627768
gjastrzebska203 <g.jastrzebska.203@studms.ug.edu.pl>
2025-05-14 15:04:02 +0200

    saved page

--- mobile/screens/SavedPage.js
+++ mobile/screens/SavedPage.js
@@ -4,16 +4,16 @@ import ThemeSwitch from '../components/themeSwitch';
 import RouteHomePage from '../components/RouteHomePage'
 import HomeFooter from '../components/HomeFooter'
 
-export default function HomePage({ navigation }) {
+export default function SavedPage({ navigation }) {
   const { colors } = useTheme();
   return (
     <View style={{ flex: 1, backgroundColor: colors.background, padding: 20 }}>
       <ThemeSwitch />
       <Text style={{ color: colors.text, fontSize: 24 }}>Home Screen</Text>
       <View>
-        <RouteHomePage />
-        <RouteHomePage />
-        <RouteHomePage />
+        <RouteSavedPage />
+        <RouteSavedPage />
+        <RouteSavedPage />
       </View>
       <HomeFooter/>
     </View>
