78266b37b972b4d27c9c74da41434feb0e2b048f
kajtek <kajtek0123@gmail.com>
2025-10-30 19:06:28 +0100

    pass service uris from application.yml files

--- services/gateway/src/main/java/com/veer/gateway/config/GatewayRoutes.java
+++ services/gateway/src/main/java/com/veer/gateway/config/GatewayRoutes.java
@@ -1,30 +1,37 @@
 package com.veer.gateway.config;
 
 import com.veer.gateway.filters.AddUserDetailsHeaderGatewayFilterFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// Gateway routes configuration.
 @Configuration
 public class GatewayRoutes {
 
     private final AddUserDetailsHeaderGatewayFilterFactory addUserDetailsHeaderFilter;
+    private final String authServiceUri;
+    private final String userServiceUri;
 
-    public GatewayRoutes(AddUserDetailsHeaderGatewayFilterFactory addUserDetailsHeaderFilter) {
+    public GatewayRoutes(
+            AddUserDetailsHeaderGatewayFilterFactory addUserDetailsHeaderFilter,
+            @Value("${services.auth.uri}") String authServiceUri,
+            @Value("${services.user.uri}") String userServiceUri) {
         this.addUserDetailsHeaderFilter = addUserDetailsHeaderFilter;
+        this.authServiceUri = authServiceUri;
+        this.userServiceUri = userServiceUri;
     }
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-            .route("auth-service", r -> r.path("/api/auth/**")
-                .uri("http://auth-service:3002"))
-            .route("user-service", r -> r.path("/api/user/**")
-                .filters(f -> f.filter(addUserDetailsHeaderFilter
-                    .apply(new AddUserDetailsHeaderGatewayFilterFactory.Config())))
-                .uri("http://user-service:3003"))
-            .build();
+                .route("auth-service", r -> r.path("/api/auth/**")
+                        .uri(authServiceUri))
+                .route("user-service", r -> r.path("/api/user/**")
+                        .filters(f -> f.filter(addUserDetailsHeaderFilter
+                                .apply(new AddUserDetailsHeaderGatewayFilterFactory.Config())))
+                        .uri(userServiceUri))
+                .build();
     }
 }
