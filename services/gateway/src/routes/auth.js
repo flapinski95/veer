649cbab2af19049e86c05b28f601e4ca34248d01
kajtek <kajtek0123@gmail.com>
2025-10-30 18:41:43 +0100

    delete old gateway app

--- services/gateway/src/routes/auth.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const createProxy = require("../utils/httpProxy");
-
-router.use("/", ...createProxy("http://auth:3002/api/auth"));
-module.exports = router;
