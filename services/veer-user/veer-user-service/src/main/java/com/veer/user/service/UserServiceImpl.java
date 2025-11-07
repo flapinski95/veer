946bc7034a2d77a1defb4ec7456f154891353b9f
kajtek <kajtek0123@gmail.com>
2025-11-07 11:37:49 +0100

    implement createUser and getUserById in UserServiceImpl

--- services/veer-user/veer-user-service/src/main/java/com/veer/user/service/UserServiceImpl.java
+++ services/veer-user/veer-user-service/src/main/java/com/veer/user/service/UserServiceImpl.java
@@ -3,6 +3,9 @@ package com.veer.user.service;
 import com.veer.user.model.ResponseUserDto;
 import com.veer.user.model.CreateUserDto;
 import com.veer.user.repository.UserRepository;
+import com.veer.user.model.exception.UserNotFoundException;
+import com.veer.user.UserMapper;
+import com.veer.user.model.User;
 
 public class UserServiceImpl implements UserService {
 
@@ -14,12 +17,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ResponseUserDto createUser(CreateUserDto createUserDto) {
-        return null;
+        User user = UserMapper.toEntity(createUserDto);
+        User savedUser = repository.save(user);
+        return UserMapper.toResponseUserDto(savedUser);
     }
 
     @Override
     public ResponseUserDto getUserById(String userId) {
-        return null;
+        User user = repository.findById(userId)
+            .orElseThrow(() -> new UserNotFoundException(
+                "User " + userId + " not found"
+            ));
+        return UserMapper.toResponseUserDto(user);
     }
 
 }
