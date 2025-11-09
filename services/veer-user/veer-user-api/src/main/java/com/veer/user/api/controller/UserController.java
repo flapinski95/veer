88ceaed231af5c034fd5c648e98cce04137b9b8f
kajtek <kajtek0123@gmail.com>
2025-11-09 18:10:08 +0100

    add getUserPrivateData and getUserPublicData in UserController

--- services/veer-user/veer-user-api/src/main/java/com/veer/user/api/controller/UserController.java
+++ services/veer-user/veer-user-api/src/main/java/com/veer/user/api/controller/UserController.java
@@ -3,13 +3,11 @@ package com.veer.user.api.controller;
 import com.veer.user.model.dto.CreateUserDto;
 import com.veer.user.model.dto.ResponseUserDto;
 import com.veer.user.service.UserService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.constraints.NotBlank;
 
@@ -41,6 +39,25 @@ public class UserController {
         ResponseUserDto createdUser = userService.createUser(createUserDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseUserDto> getUserPrivateData(
+        @RequestHeader("X-User-Id") @NotBlank String userId
+    ) {
+        ResponseUserDto user = userService.getUserById(userId);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ResponseUserDto> getUserPublicData(
+        @PathVariable @NotBlank String userId
+    ) {
+        ResponseUserDto user = userService.getUserById(userId);
+
+        user.setEmail(null); // for now the only private data is the email
+
+        return ResponseEntity.ok(user);
     }
 
 }
