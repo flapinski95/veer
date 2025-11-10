23cd40cf1907d6481144d42f04216a7309123a33
Kajetan Lach <kajetan.lach@frisbii.com>
2025-11-10 09:27:55 +0100

    add following relationships related exceptions

---
+++ services/veer-user/veer-user-model/src/main/java/com/veer/user/model/exception/FollowingAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.veer.user.model.exception;
+
+public class FollowingAlreadyExistsException extends RuntimeException {
+    
+    public FollowingAlreadyExistsException(String message) { super(message); }
+
+}
