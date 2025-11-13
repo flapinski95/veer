4791937e463f7cd12d14522c80ad2d520815dc66
kajtek <kajtek0123@gmail.com>
2025-11-13 17:09:37 +0100

    add component scan to the app entry point

--- services/veer-user/veer-user-api/src/main/java/com/veer/user/api/VeerUserApplication.java
+++ services/veer-user/veer-user-api/src/main/java/com/veer/user/api/VeerUserApplication.java
@@ -2,8 +2,10 @@ package com.veer.user.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.veer.user")
 public class VeerUserApplication {
 
     public static void main(String[] args) {
