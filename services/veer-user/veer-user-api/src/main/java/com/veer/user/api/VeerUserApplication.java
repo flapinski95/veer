22dcf5c23032518f7916b61afce33ffed9230280
kajtek <kajtek0123@gmail.com>
2025-11-14 14:35:02 +0100

    add jpa repository & entity scan in main user service entry point

--- services/veer-user/veer-user-api/src/main/java/com/veer/user/api/VeerUserApplication.java
+++ services/veer-user/veer-user-api/src/main/java/com/veer/user/api/VeerUserApplication.java
@@ -2,10 +2,14 @@ package com.veer.user.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.veer.user")
+@ComponentScan        (basePackages = "com.veer.user")
+@EnableJpaRepositories(basePackages = "com.veer.user.repository")
+@EntityScan           (basePackages = "com.veer.user.model")
 public class VeerUserApplication {
 
     public static void main(String[] args) {
